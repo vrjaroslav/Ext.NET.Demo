ae3513fdc176df5740c75341aabe9441e520023f
geoffreymcgill <geoff@ext.net>
2013-04-21 04:38:21 -0600

    Updated .Seed data creation
    Signed-off-by: geoffreymcgill <geoff@ext.net>

--- UberBaker/Uber.Data/Migrations/Configuration.cs
+++ UberBaker/Uber.Data/Migrations/Configuration.cs
@@ -13,25 +13,31 @@ namespace Uber.Data
 
         protected override void Seed(UberContext data)
         {
-            var products = new List<Product> { 
-                new Product { Name = "Sour Dough", ShortCode="sourdough" },
-                new Product { Name = "5 Grain", ShortCode="5grain" },
-                new Product { Name = "Baguette", ShortCode="baguette" },
-                new Product { Name = "Dark Rye", ShortCode="darkrye" },
-                new Product { Name = "Pumpernickel", ShortCode="pumpernickel" },
-                new Product { Name = "Corn Bread", ShortCode="cornbread" },
-                new Product { Name = "Brioche", ShortCode="brioche" },
-                new Product { Name = "Focaccia", ShortCode="focaccia" }
-            };
-
-            products.ForEach(product => data.Products.AddOrUpdate(item => item.ShortCode, product));
+            var bread = new ProductType { Name = "Bread", ShortCode = "bread" };
+            var pastry = new ProductType { Name = "Pastry", ShortCode = "pastry" };
 
             var productTypes = new List<ProductType>
             {
-                new ProductType { Name = "Bread", ShortCode="bread" }
+                bread,
+                pastry,
             };
 
             productTypes.ForEach(productType => data.ProductTypes.AddOrUpdate(item => item.ShortCode, productType));
+
+            var products = new List<Product> { 
+                new Product { Name = "Sour Dough", UnitPrice = 2.22, ShortCode = "sourdough" },
+                new Product { Name = "5 Grain", UnitPrice = 3.33, ShortCode = "5grain" },
+                new Product { Name = "Baguette", ShortCode = "baguette", Type = bread },
+                new Product { Name = "Dark Rye", UnitPrice = 4.44, ShortCode = "darkrye", Type = bread },
+                new Product { Name = "Pumpernickel", ShortCode = "pumpernickel" },
+                new Product { Name = "Corn Bread", UnitPrice = 5.55, ShortCode = "cornbread" },
+                new Product { Name = "Brioche", ShortCode = "brioche" },
+                new Product { Name = "Focaccia", ShortCode = "focaccia" },
+                new Product { Name = "Croissant", ShortCode = "croissant", Type = pastry },
+                new Product { Name = "Pain au Chocolat", UnitPrice = 1.11, ShortCode = "painauchocolat", Type = pastry },
+            };
+
+            products.ForEach(product => data.Products.AddOrUpdate(item => item.ShortCode, product));
 
             // IMPORTANT!!
             data.SaveChanges();
